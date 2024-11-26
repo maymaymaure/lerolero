67d4d51cdc3af28924f91b7bc04b9d567652d806
maymaymaure <00001113235834sp@al.educacao.sp.gov.br>
2024-11-26 07:35:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Países',
+    'Que país tem o formato de uma bota?',
+    'Italia'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'corpo humano',
+    'Quantos ossos temos no nosso corpo?',
+    '206'
+)
+
+criaCartao(
+    'geografia',
+    'Quantos continentes existem?',
+    '6 continentes'
+)
